2782c1f64d5ea66eed0975593253ad8a24c69560
Pavan S <pavan@bambu.co>
2022-10-17 22:41:37 +0530

    ✨ fast2sms integration

--- src/api.js
+++ src/api.js
@@ -2,7 +2,10 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
+app.use(express.json());
 const router = express.Router();
+const unirest = require("unirest");
+const request = unirest("POST", "https://www.fast2sms.com/dev/bulkV2");
 
 const cors = require("cors");
 app.use(
@@ -11,9 +14,26 @@ app.use(
   })
 );
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "Hello, world!",
+router.post("/send-message", (req, res) => {
+  const { numbers, message } = req.body;
+  const API_KEY =
+    "j1slTIAql2P5nNRWk5FgRbs1iWZ9LvDfRL8nMm4tF5ynihvw3vlRWXNPi9fF";
+
+  request.headers({
+    authorization: API_KEY,
+  });
+
+  request.form({
+    numbers,
+    message,
+    route: "q",
+    sender_id: "TXTIND",
+    language: "english",
+  });
+
+  request.end(function (data) {
+    if (data.error) console.log("error at otp");
+    res.send(data.body);
   });
 });
 
